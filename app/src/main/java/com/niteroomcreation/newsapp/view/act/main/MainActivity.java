e73996a8085bba7986cc779c16cb2d1cb2a89a43
Septian Adi Wijaya <developadi@gmail.com>
2021-04-27 14:11:37 +0700

    navigation flow - passing parameter fragment

--- app/src/main/java/com/niteroomcreation/newsapp/view/act/main/MainActivity.java
+++ app/src/main/java/com/niteroomcreation/newsapp/view/act/main/MainActivity.java
@@ -1,8 +1,13 @@
 package com.niteroomcreation.newsapp.view.act.main;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.niteroomcreation.newsapp.R;
 import com.niteroomcreation.newsapp.databinding.AMainBinding;
 import com.niteroomcreation.newsapp.view.BaseActivity;
@@ -13,6 +18,7 @@ public class MainActivity extends BaseActivity {
 
     private AMainBinding binding;
     private NavHostFragment navHostFragment;
+    private int selectedFragmentID;
 
     @Override
     public void onCreateInside() {
@@ -29,5 +35,46 @@ public class MainActivity extends BaseActivity {
     private void setupNavigation() {
         navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_fragment);
         NavigationUI.setupWithNavController(binding.navBottom, navHostFragment.getNavController());
+
+        binding.navBottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                Bundle b = new Bundle();
+
+                switch (menuItem.getItemId()) {
+                    case R.id.action_m_search:
+                        selectedFragmentID = menuItem.getItemId();
+
+                        b.putString("m_key", "SEARCH");
+                        navHostFragment.getNavController().navigate(R.id.action_m_search, b);
+
+                        return true;
+
+                    case R.id.action_m_bookmark:
+                        selectedFragmentID = menuItem.getItemId();
+
+                        b.putString("m_key", "BOOKMARK");
+                        navHostFragment.getNavController().navigate(R.id.action_m_bookmark, b);
+                        return true;
+
+                    case R.id.action_m_dashboard:
+                        selectedFragmentID = menuItem.getItemId();
+
+                        b.putString("m_key", "DASHBOARD");
+                        navHostFragment.getNavController().navigate(R.id.action_m_dashboard, b);
+                        return true;
+
+                    case R.id.action_m_setting:
+                        selectedFragmentID = menuItem.getItemId();
+
+                        navHostFragment.getNavController().navigate(R.id.action_m_setting);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        binding.navBottom.setSelectedItemId(navHostFragment.getNavController().getCurrentDestination().getId());
     }
 }
---
+++ app/src/main/java/com/niteroomcreation/newsapp/view/fragment/EmptyFragment.java
@@ -0,0 +1,50 @@
+package com.niteroomcreation.newsapp.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.niteroomcreation.newsapp.databinding.FEmptyBinding;
+import com.niteroomcreation.newsapp.util.LogHelper;
+import com.niteroomcreation.newsapp.view.BaseFragment;
+
+/**
+ * Created by monta on 27/04/21
+ * please make sure to use credit when using people code
+ **/
+public class EmptyFragment extends BaseFragment {
+
+    public static final String TAG = EmptyFragment.class.getSimpleName();
+
+    private FEmptyBinding binding;
+    private String title;
+
+    @Override
+    public View onInflateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        binding = FEmptyBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void initUI() {
+        if (getArguments() != null) {
+            LogHelper.e(TAG, getArguments().get("m_key"));
+
+            binding.emptyTv.setText(getArguments().get("m_key").toString());
+        } else {
+            LogHelper.e(TAG, "argument is empty");
+
+            binding.emptyTv.setText("KOSONG");
+        }
+    }
+
+    @Override
+    public void destroyUI() {
+        binding = null;
+    }
+}
